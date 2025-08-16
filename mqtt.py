0135acee769f2ad74daab564cf4b91347ad75542
Deosrc <deosrc@g-de.co.uk>
2025-08-16 23:03:56 +0100

    Add MQTT messaging

--- mqtt.py
+++ mqtt.py
@@ -1,10 +1,28 @@
+import os
 from NovaApi.ListDevices.nbe_list_devices import get_devices
 from NovaApi.ExecuteAction.LocateTracker.location_request import get_location_data_for_device
+from NovaApi.ExecuteAction.LocateTracker.decrypted_location import WrappedLocation
+from mqtt_client import MqttClient
+
+
+mqtt = MqttClient(
+    os.environ["MQTT_HOST"],
+    os.environ["MQTT_USERNAME"],
+    os.environ["MQTT_PASSWORD"])
 
 def update_device(device_name, canonic_id):
     print(f"Updating device {device_name}...")
-    locations = get_location_data_for_device(canonic_id, device_name)
-    print(locations)
+    try:
+        locations = get_location_data_for_device(canonic_id, device_name)
+
+        if locations:
+            latest_location = next(iter(sorted(iter(locations), key=lambda x: x.time, reverse=True)), None)
+
+            print("Sending device update to MQTT...")
+            mqtt.send_update(canonic_id, device_name, latest_location)
+    except Exception as err:
+        print(f"Error updating device: {err}")
+        mqtt.send_error_update(canonic_id, device_name, err)
 
 def update_mqtt():
     print("Refreshing device list...")
---
+++ mqtt_client.py
@@ -0,0 +1,80 @@
+from enum import Enum
+from paho.mqtt.enums import MQTTErrorCode, MQTTProtocolVersion
+from random import randint
+import json
+import paho.mqtt.client as mqtt
+
+from NovaApi.ExecuteAction.LocateTracker.decrypted_location import WrappedLocation
+
+class ConnectionStatus(Enum):
+    ERROR = -1
+    DISCONNECTED = 0
+    CONNECTED = 1
+
+class MqttClient:
+
+    TOPIC_TYPE_NAME = "name"
+    TOPIC_TYPE_LOCATION = "location"
+
+    def __init__(self, host: str, username: str, password: str):
+        self._host = host
+
+        self._connection_status = ConnectionStatus.DISCONNECTED
+        self._client_id = f'google-find-{randint(0, 65535)}'
+
+        self._client = mqtt.Client(
+            mqtt.CallbackAPIVersion.VERSION2,
+            client_id=self._client_id,
+            protocol=MQTTProtocolVersion.MQTTv5)
+        self._client.username_pw_set(username, password)
+
+    def send_update(self, canonic_id: str, device_name: str, location: WrappedLocation) -> None:
+        self._ensure_connected()
+        if self._connection_status != ConnectionStatus.CONNECTED:
+            return
+
+        # Publish device information
+        self._client.publish(
+            self._get_topic(canonic_id, MqttClient.TOPIC_TYPE_NAME),
+            device_name)
+        self._client.publish(
+            self._get_topic(canonic_id, MqttClient.TOPIC_TYPE_LOCATION),
+            json.dumps({
+                "latitude": location.latitude,
+                "longitude": location.longitude,
+                "altitude": location.altitude
+            }))
+
+        # Publish Home Assistant discovery topic
+        self._client.publish(
+            f"homeassistant/device_tracker/googlefind/{canonic_id}/config",
+            json.dumps({
+                "name": device_name,
+                "json_attributes_topic": self._get_topic(canonic_id, MqttClient.TOPIC_TYPE_LOCATION),
+            }))
+
+    def send_error_update(self, canonic_id, device_name, err) -> None:
+        self._ensure_connected()
+        if self._connection_status != ConnectionStatus.CONNECTED:
+            return
+
+        # Publish device information
+        self._client.publish(
+            self._get_topic(canonic_id, MqttClient.TOPIC_TYPE_NAME),
+            device_name)
+        self._client.publish(
+            self._get_topic(canonic_id, MqttClient.TOPIC_TYPE_ERROR),
+            err)
+
+    def _ensure_connected(self):
+        if self._connection_status == ConnectionStatus.CONNECTED:
+            return
+
+        result = self._client.connect(self._host)
+        if result == MQTTErrorCode.MQTT_ERR_SUCCESS:
+            self._connection_status = ConnectionStatus.CONNECTED
+        else:
+            self._connection_status == ConnectionStatus.ERROR
+
+    def _get_topic(self, canonic_id: str, type: str) -> str:
+        return f"google-find/{canonic_id}/location"
