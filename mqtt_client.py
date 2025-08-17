d341610ef9849a1a1e0f9ed6063d473130463f50
Deosrc <deosrc@g-de.co.uk>
2025-08-17 01:23:01 +0100

    Add source type

--- mqtt_client.py
+++ mqtt_client.py
@@ -53,6 +53,7 @@ class MqttClient:
                 "unique_id": canonic_id,
                 "name": device_name,
                 "json_attributes_topic": self._get_topic(canonic_id, MqttClient.TOPIC_TYPE_LOCATION),
+                "source_type": "bluetooth_le"
             }))
 
     def send_error_update(self, canonic_id, device_name, err) -> None:
